e7c79ffd5b770207588091ddd9d6d661abd08804
Uwem Dickson <dicksonunique@gmail.com>
2022-11-30 15:48:52 -0800

    correcting indentation

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+# Author - UD
 
-def magic_calculation(a, b, c): 
+def magic_calculation(a, b, c):
+    """Match bytecode provided by Holberton School."""
     if a < b:
         return (c)
     if c > b:
